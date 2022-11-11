cbe50edf34dc54018e206114dcfa350984351739
polluque <polluque26081997@gmail.com>
2022-11-11 11:14:11 -0500

    flutter_components_v1.7

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardPage extends StatelessWidget {
@@ -115,49 +117,126 @@ class CardPage extends StatelessWidget {
                   offset: Offset(-4, -4),
                   blurRadius: 6.0,
                 ),
-              ],
               
+              ],
               ),
-            child: Column(
-              children: [
-                Text(
-                  'colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-https://loremipsum.io/',
-                textAlign: TextAlign.center,
-                //cantidad de lineas que se quieren mostrar
-                maxLines: 5,
-                overflow: TextOverflow.ellipsis,
+              child: Row(
+                children: [
+                Image.asset(
+                  'assets/images/undraw_Dog_re_7980.png',
+                  height: 130.0,
+                  width: 130.0,
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 12.0, 
+                // el siguiente widget column se recubre con un expanded para adecuar el texto y cubrir todo el container 
+                SizedBox(
+                    width: 5.0,
                   ),
-                  width: double.infinity,
-                  height: 40.0,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color:Colors.yellowAccent,
-                    borderRadius: BorderRadius.circular(18.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        offset: Offset(4, 4),
-                        blurRadius: 6.0,
-                      ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        'fiorela guadalupe de las nieves azules',
+                     style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                     ),
+                     ),
+                     // generando un espacio entre fiorela y colocar 
+
+                     SizedBox(
+                      height: 7.0,
+                     ),
+                     Text('colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-https://loremipsum.io/',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 4,
+                    textAlign: TextAlign.right,
+                     ),
                     ],
                   ),
-                  child: 
-                Text(
-                  'follow me ',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.0, 
                 ),
+                ],
+              ),
+          ),
+        
+        // card 3
+         Container(
+            // espacio entre el borde de la pantlla y el container 
+            margin: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(10.0),
+            //color: Colors.blue,
+            
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(18.0),
+
+              boxShadow: [
+                BoxShadow(
+                  color:Colors.black.withOpacity(0.3),
+                  offset: Offset(4, 4),
+                  blurRadius: 12.0,
                 ),
-                
+              BoxShadow(
+                  color:Colors.white.withOpacity(0.6),
+                  offset: Offset(-4, -4),
+                  blurRadius: 6.0,
                 ),
+              
               ],
-            ),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-https://loremipsum.io/',
+                    maxLines: 6,
+                    overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  // espacio entre la imagen y el texto
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                // borde redondeado de la imagen, recubrimos con un widget
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image.network(
+                    'https://thumbs.dreamstime.com/b/hola-¡cierre-de-la-palabra-en-español-escrito-mano-una-pizarra-167996773.jpg',
+                    height: 130.0,
+                    // este es el responsable de no ver redondeos en la imagen 
+                    //width: 130.0,
+                     
+                    
+                  ),
+                ),
+                // el siguiente widget column se recubre con un expanded para adecuar el texto y cubrir todo el container 
+                /*Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        'fiorela guadalupe de las nieves azules',
+                     style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                     ),
+                     ),
+                     // generando un espacio entre fiorela y colocar 
+
+                     SizedBox(
+                      height: 7.0,
+                     ),
+                     Text('colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-colocar cualquier texto, utilizar un generador de texto online-https://loremipsum.io/',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 4,
+                    textAlign: TextAlign.right,
+                     ),
+                    ],
+                  ),
+                ),
+                */
+                ],
+              ),
           ),
         ],
       ),
